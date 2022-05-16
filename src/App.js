ae84241c997f725ce36afa9e895543597cc9d32a
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2022-05-16 21:33:26 +0900

    データの保存

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 
+const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
+const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+
 function GithubUser({ login, token }) {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJSON(`user:${login}`));
+
+  useEffect(() => {
+    if (!data) return;
+    // console.log("data");
+    // if (data.login === login) return;
+    // console.log("save");
+    const { name, avatar_url, location } = data;
+    saveJSON(`user:${login}`, {
+      name,
+      login,
+      avatar_url,
+      location,
+    });
+  }, [data]);
 
   useEffect(() => {
     if (!login) return;
+    if (data && data.login === login) return;
     fetch(`https://api.github.com/users/${login}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
@@ -13,7 +31,7 @@ function GithubUser({ login, token }) {
       .then((response) => response.json())
       .then(setData)
       .catch((error) => console.log(error));
-  }, [login, token]);
+  }, [login]);
 
   if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
   // console.log("null");
@@ -24,7 +42,7 @@ function App() {
   return (
     <GithubUser
       login="takafumiwao"
-      token="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"
+      token="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"
     />
   );
 }
