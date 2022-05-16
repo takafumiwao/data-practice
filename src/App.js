9c1357a23059a9c44c78c1c9f74102193f187eea
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2022-05-16 21:11:00 +0900

    リクエストの認証

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function GithubUser({ login, token }) {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (!login) return;
+    fetch(`https://api.github.com/users/${login}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then(setData)
+      .catch((error) => console.log(error));
+  }, [login, token]);
+
+  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  // console.log("null");
+  return null;
+}
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <GithubUser
+      login="takafumiwao"
+      token="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"
+    />
+  );
 }
 
 export default App;
